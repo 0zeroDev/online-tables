b513d587308fa8df2f404e17fd11c06c38764b64
Lavr Brichkin <wavilonthebest@gmail.com>
2023-02-23 21:24:58 +0100

    Fix for frontend

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ from flask import render_template
 
 from db_methods import Database
 
-app = Flask(__name__, template_folder="../frontend/")
+app = Flask(__name__, template_folder='../frontend/')
+
 
 db = Database(engine='sqlite:///online-calc.db')
 
@@ -14,18 +15,13 @@ for raw in table:
 
 @app.route('/')
 def index():
-    html = render_template(
-        'base.html',
+    return render_template(
+        'table.html',
         title='Online Tables',
-        cells=[["1","1"], ["srtg", "ddes"]],
+        cells=table
     )
-    return html
 
 
 
-# if __name__ == "__main__":
-#     db = Database(engine='sqlite:///online-calc.db')
-#     table = db.create_empty_table(2, 2)
-#     for raw in table:
-#         db.fill_table(raw)
-#     db.parse_cells()
+if __name__ == "__main__":
+    app.run(debug=True)
